e8379b935e75a58bd47f56f717319ac84e3d94ed
Sagar Band <sgadave18@gmail.com>
2023-01-15 07:44:43 +0530

    Adding static files

---
+++ js/main.js
@@ -0,0 +1,126 @@
+let localStream;
+let remoteStream;
+
+const APP_ID = "38f99d48b8054706bb4ba6d7c03845ae"
+let token = null;
+
+let uid = String(Math.floor(Math.random() * 10000));
+
+let client;
+let channel;
+
+
+const servers = {
+    iceServers: [
+        {
+            urls: [
+                "stun:stun1.l.google.com:19302",
+                "stun:stun2.l.google.com:19302"
+            ]
+        }
+    ]
+}
+
+let init = async () => {
+
+    client = await AgoraRTM.createInstance(APP_ID);
+    await client.login({ uid, token });
+
+    channel = client.createChannel("main");
+    await channel.join();
+
+    channel.on('MemberJoined', handleUserJoined)
+
+    client.on('MessageFromPeer', handleMessageFromPeer)
+
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    document.getElementById('user1').srcObject = localStream;
+
+}
+
+let handleMessageFromPeer = async (message, MemberId) => {
+
+    message = JSON.parse(message.text);
+
+    if (message.type === 'offer') {
+        createAnswer(MemberId, message.offer);
+    }
+
+    if (message.type === 'answer') {
+        addAnswer(message.answer);
+    }
+
+    if (message.type === 'candidate') {
+        if(peerConnection){
+            peerConnection.addIceCandidate(message.candidate);
+        }
+    }
+}
+
+
+let handleUserJoined = async (MemberId) => {
+    console.log("New User Joined : ", MemberId);
+    createOffer(MemberId);
+}
+
+let createPeerConnection = async (MemberId) => {
+    peerConnection = new RTCPeerConnection(servers);
+    remoteStream = new MediaStream();
+    document.getElementById('user2').srcObject = remoteStream
+
+
+    if (!localStream) {
+        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        document.getElementById('user1').srcObject = localStream;
+    }
+
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream)
+    })
+
+    peerConnection.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+            remoteStream.addTrack(track)
+        })
+    }
+
+    peerConnection.onicecandidate = async (event) => {
+        if (event.candidate) {
+            client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'candidate', 'candidate': event.candidate }) }, MemberId);
+            console.log('New ICE candidate : ', event.candidate);
+        }
+    }
+}
+
+let createOffer = async (MemberId) => {
+
+    await createPeerConnection(MemberId);
+
+    let offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+
+    client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'offer', 'offer': offer }) }, MemberId);
+
+
+}
+
+
+let createAnswer = async (MemberId, offer) => {
+
+    await createPeerConnection(MemberId);
+    await peerConnection.setRemoteDescription(offer);
+
+    let answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+
+    client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'answer', 'offer': answer }) }, MemberId);
+
+}
+
+let addAnswer = async (answer)=>{
+    if(!peerConnection.currentRemoteDescription){
+        peerConnection.setRemoteDescription(answer);
+    }
+}
+
+init();
