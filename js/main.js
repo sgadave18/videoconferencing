c7da02d73de64c4c91fe833f40c6fb131be42924
Sagar Band <sgadave18@gmail.com>
2023-01-15 09:20:42 +0530

    Updated Main JS

--- js/main.js
+++ js/main.js
@@ -1,87 +1,100 @@
-let localStream;
-let remoteStream;
-
 const APP_ID = "38f99d48b8054706bb4ba6d7c03845ae"
-let token = null;
 
-let uid = String(Math.floor(Math.random() * 10000));
+let token = null;
+let uid = String(Math.floor(Math.random() * 10000))
 
 let client;
 let channel;
 
+let queryString = window.location.search
+let urlParams = new URLSearchParams(queryString)
+let roomId = 'main'
+
+
+
+let localStream;
+let remoteStream;
+let peerConnection;
 
 const servers = {
-    iceServers: [
+    iceServers:[
         {
-            urls: [
-                "stun:stun1.l.google.com:19302",
-                "stun:stun2.l.google.com:19302"
-            ]
+            urls:['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
         }
     ]
 }
 
+
+let constraints = {
+    video:{
+        width:{min:640, ideal:1920, max:1920},
+        height:{min:480, ideal:1080, max:1080},
+    },
+    audio:true
+}
+
 let init = async () => {
+    client = await AgoraRTM.createInstance(APP_ID)
+    await client.login({uid, token})
 
-    client = await AgoraRTM.createInstance(APP_ID);
-    await client.login({ uid, token });
-
-    channel = client.createChannel("main");
-    await channel.join();
+    channel = client.createChannel(roomId)
+    await channel.join()
 
     channel.on('MemberJoined', handleUserJoined)
+    channel.on('MemberLeft', handleUserLeft)
 
     client.on('MessageFromPeer', handleMessageFromPeer)
 
-    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-    document.getElementById('user1').srcObject = localStream;
-    console.log("LOCAL STREAM++++++++++",localStream);
+    localStream = await navigator.mediaDevices.getUserMedia(constraints)
+    document.getElementById('user1').srcObject = localStream
+}
+ 
 
+let handleUserLeft = (MemberId) => {
+    document.getElementById('user2').style.display = 'none'
+    document.getElementById('user1').classList.remove('smallFrame')
 }
 
 let handleMessageFromPeer = async (message, MemberId) => {
 
-    message = JSON.parse(message.text);
-    console.log("MESSEGE FROM PEER----",message);
+    message = JSON.parse(message.text)
 
-    if (message.type === 'offer') {
-        console.log("IN MESSEGE TYPE OFFER**************************")
-        createAnswer(MemberId, message.offer);
+    if(message.type === 'offer'){
+        createAnswer(MemberId, message.offer)
     }
 
-    if (message.type === 'answer') {
-        addAnswer(message.answer);
+    if(message.type === 'answer'){
+        addAnswer(message.answer)
     }
 
-    if (message.type === 'candidate') {
-        if (peerConnection) {
-            // console.log("RTCPEERCONNECTION REMOTE DESCRIPTION----******",peerConnection.currentRemoteDescription);
-            // if (peerConnection.currentRemoteDescription) {
-                peerConnection.addIceCandidate(message.candidate);
-            // }
-
+    if(message.type === 'candidate'){
+        if(peerConnection){
+            peerConnection.addIceCandidate(message.candidate)
         }
     }
-}
 
+
+}
 
 let handleUserJoined = async (MemberId) => {
-    // console.log("New User Joined : ", MemberId);
-    createOffer(MemberId);
+    console.log('A new user joined the channel:', MemberId)
+    createOffer(MemberId)
 }
 
+
 let createPeerConnection = async (MemberId) => {
-console.log("IN CREATE PEER CONNECTION***********")
-    peerConnection = new RTCPeerConnection(servers);
+    peerConnection = new RTCPeerConnection(servers)
 
-    remoteStream = new MediaStream();
-    console.log("IN CREATE PEER CONNECTION REMOTE STREAM***********-----",remoteStream);
+    remoteStream = new MediaStream()
     document.getElementById('user2').srcObject = remoteStream
+    document.getElementById('user2').style.display = 'block'
+
+    document.getElementById('user1').classList.add('smallFrame')
 
 
-    if (!localStream) {
-        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-        document.getElementById('user1').srcObject = localStream;
+    if(!localStream){
+        localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+        document.getElementById('user1').srcObject = localStream
     }
 
     localStream.getTracks().forEach((track) => {
@@ -95,43 +108,73 @@ console.log("IN CREATE PEER CONNECTION***********")
     }
 
     peerConnection.onicecandidate = async (event) => {
-        if (event.candidate) {
-            client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'candidate', 'candidate': event.candidate }) }, MemberId);
-            // console.log('New ICE candidate : ', event.candidate);
+        if(event.candidate){
+            client.sendMessageToPeer({text:JSON.stringify({'type':'candidate', 'candidate':event.candidate})}, MemberId)
         }
     }
 }
 
 let createOffer = async (MemberId) => {
+    await createPeerConnection(MemberId)
 
-    await createPeerConnection(MemberId);
+    let offer = await peerConnection.createOffer()
+    await peerConnection.setLocalDescription(offer)
 
-    let offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(offer);
-
-    client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'offer', 'offer': offer }) }, MemberId);
-
-
+    client.sendMessageToPeer({text:JSON.stringify({'type':'offer', 'offer':offer})}, MemberId)
 }
 
 
 let createAnswer = async (MemberId, offer) => {
-    console.log("IN CREATE ANSWER **********")
-    await createPeerConnection(MemberId);
-    console.log("IN CREATE ANSWER AFTER CREATE PEER CONNECTION***********")
-    await peerConnection.setRemoteDescription(offer);
-    console.log("IN CREATE ANSWER REMOTE DESCRIPTION-----",peerConnection.currentRemoteDescription)
-    let answer = await peerConnection.createAnswer();
-    await peerConnection.setLocalDescription(answer);
-    console.log("IN CREATE ANSWER ----",peerConnection.currentRemoteDescription)
-    client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'answer', 'offer': answer }) }, MemberId);
+    await createPeerConnection(MemberId)
 
+    await peerConnection.setRemoteDescription(offer)
+
+    let answer = await peerConnection.createAnswer()
+    await peerConnection.setLocalDescription(answer)
+
+    client.sendMessageToPeer({text:JSON.stringify({'type':'answer', 'answer':answer})}, MemberId)
 }
 
+
 let addAnswer = async (answer) => {
-    if (!peerConnection.currentRemoteDescription) {
-        peerConnection.setRemoteDescription(answer);
+    if(!peerConnection.currentRemoteDescription){
+        peerConnection.setRemoteDescription(answer)
     }
 }
 
-init();
+
+let leaveChannel = async () => {
+    await channel.leave()
+    await client.logout()
+}
+
+let toggleCamera = async () => {
+    let videoTrack = localStream.getTracks().find(track => track.kind === 'video')
+
+    if(videoTrack.enabled){
+        videoTrack.enabled = false
+        document.getElementById('camera-btn').style.backgroundColor = 'rgb(255, 80, 80)'
+    }else{
+        videoTrack.enabled = true
+        document.getElementById('camera-btn').style.backgroundColor = 'rgb(179, 102, 249, .9)'
+    }
+}
+
+let toggleMic = async () => {
+    let audioTrack = localStream.getTracks().find(track => track.kind === 'audio')
+
+    if(audioTrack.enabled){
+        audioTrack.enabled = false
+        document.getElementById('mic-btn').style.backgroundColor = 'rgb(255, 80, 80)'
+    }else{
+        audioTrack.enabled = true
+        document.getElementById('mic-btn').style.backgroundColor = 'rgb(179, 102, 249, .9)'
+    }
+}
+  
+// window.addEventListener('beforeunload', leaveChannel)
+
+// document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+// document.getElementById('mic-btn').addEventListener('click', toggleMic)
+
+init()
