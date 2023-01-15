81443f5d9a499b1d7df4a2dfeea05722c3648497
Sagar Band <sgadave18@gmail.com>
2023-01-15 08:32:46 +0530

    updated main.js

--- js/main.js
+++ js/main.js
@@ -35,14 +35,17 @@ let init = async () => {
 
     localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
     document.getElementById('user1').srcObject = localStream;
+    console.log("LOCAL STREAM++++++++++",localStream);
 
 }
 
 let handleMessageFromPeer = async (message, MemberId) => {
 
     message = JSON.parse(message.text);
+    console.log("MESSEGE FROM PEER----",message);
 
     if (message.type === 'offer') {
+        console.log("IN MESSEGE TYPE OFFER**************************")
         createAnswer(MemberId, message.offer);
     }
 
@@ -51,21 +54,28 @@ let handleMessageFromPeer = async (message, MemberId) => {
     }
 
     if (message.type === 'candidate') {
-        if(peerConnection){
-            peerConnection.addIceCandidate(message.candidate);
+        if (peerConnection) {
+            // console.log("RTCPEERCONNECTION REMOTE DESCRIPTION----******",peerConnection.currentRemoteDescription);
+            // if (peerConnection.currentRemoteDescription) {
+                peerConnection.addIceCandidate(message.candidate);
+            // }
+
         }
     }
 }
 
 
 let handleUserJoined = async (MemberId) => {
-    console.log("New User Joined : ", MemberId);
+    // console.log("New User Joined : ", MemberId);
     createOffer(MemberId);
 }
 
 let createPeerConnection = async (MemberId) => {
+console.log("IN CREATE PEER CONNECTION***********")
     peerConnection = new RTCPeerConnection(servers);
+
     remoteStream = new MediaStream();
+    console.log("IN CREATE PEER CONNECTION REMOTE STREAM***********-----",remoteStream);
     document.getElementById('user2').srcObject = remoteStream
 
 
@@ -87,7 +97,7 @@ let createPeerConnection = async (MemberId) => {
     peerConnection.onicecandidate = async (event) => {
         if (event.candidate) {
             client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'candidate', 'candidate': event.candidate }) }, MemberId);
-            console.log('New ICE candidate : ', event.candidate);
+            // console.log('New ICE candidate : ', event.candidate);
         }
     }
 }
@@ -106,19 +116,20 @@ let createOffer = async (MemberId) => {
 
 
 let createAnswer = async (MemberId, offer) => {
-
+    console.log("IN CREATE ANSWER **********")
     await createPeerConnection(MemberId);
+    console.log("IN CREATE ANSWER AFTER CREATE PEER CONNECTION***********")
     await peerConnection.setRemoteDescription(offer);
-
+    console.log("IN CREATE ANSWER REMOTE DESCRIPTION-----",peerConnection.currentRemoteDescription)
     let answer = await peerConnection.createAnswer();
     await peerConnection.setLocalDescription(answer);
-
+    console.log("IN CREATE ANSWER ----",peerConnection.currentRemoteDescription)
     client.sendMessageToPeer({ text: JSON.stringify({ 'type': 'answer', 'offer': answer }) }, MemberId);
 
 }
 
-let addAnswer = async (answer)=>{
-    if(!peerConnection.currentRemoteDescription){
+let addAnswer = async (answer) => {
+    if (!peerConnection.currentRemoteDescription) {
         peerConnection.setRemoteDescription(answer);
     }
 }
